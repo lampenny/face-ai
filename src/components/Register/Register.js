a6671ad954197f5da421b71cb2d19d98bdf2a098
Penny Lam <pennylam@live.com>
2021-07-12 17:28:44 +0900

    fix

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -68,7 +68,7 @@ class Register extends React.Component {
                       onChange={this.onEmailChange}
                     />
                 </div>
-                <div class="mv3">
+                <div className="mv3">
                     <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                     <input
                       className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
